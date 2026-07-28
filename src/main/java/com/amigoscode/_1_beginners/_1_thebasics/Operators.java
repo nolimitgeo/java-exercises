fbdad0e5b19b49b6479ef7cf7cc59487302fa4df
nolimitgeo <jobanytorres17@gmail.com>
2026-07-28 12:08:06 -0700

    Operators exercise completed

--- src/main/java/com/amigoscode/_1_beginners/_1_thebasics/Operators.java
+++ src/main/java/com/amigoscode/_1_beginners/_1_thebasics/Operators.java
@@ -13,27 +13,51 @@ public class Operators {
         // TODO: 1 - Use arithmetic operators (+, -, *, /) on two int variables and print the results
         // Declare two int variables (e.g., a = 10, b = 3)
         // Print the result of a + b, a - b, a * b, and a / b
-
+        int length = 5;
+        int width = 4;
+        int area = length * width;
+        System.out.println(area);
 
         // TODO: 2 - Use the modulus operator (%) to check if a number is even
         // Declare an int variable called number with any value.
         // Print the result of number % 2
         // Print whether the number is even (result is 0) or odd (result is 1)
+        int number = 2;
 
+        if (number % 2 == 0) {
+            System.out.println("Number is even");
+        } else {
+            System.out.println("Number is odd");
+        }
 
         // TODO: 3 - Use increment (++) and decrement (--) operators
         // Declare an int variable called counter, set it to 5
         // Use counter++ and print the result, then use counter-- and print the result
-
+        int counter = 5;
+        System.out.println(counter++);
+        System.out.println(counter--);
 
         // TODO: 4 - Use compound assignment operators (+=, -=, *=)
         // Declare an int variable called score, set it to 10
         // Use +=, -=, and *= on score, printing after each operation
+        int score = 10;
+
+        System.out.println(score += 2);
+        System.out.println(score -= 2);
+        System.out.println(score *= 2);
 
 
         // TODO: 5 - Use comparison operators (==, !=, >, <, >=, <=) and print the boolean results
         // Declare two int variables (e.g., x = 5, y = 10)
         // Print the result of each comparison, e.g.: System.out.println("x == y: " + (x == y));
+        int x = 5;
+        int y = 10;
+        System.out.println("x == y: " + (x == y));
+        System.out.println("x != y: " + (x != y));
+        System.out.println("x > y: " + (x > y));
+        System.out.println("x < y: " + (x < y));
+        System.out.println("x >= y: " + (x >= y));
+        System.out.println("x <= y: " + (x <= y));
 
 
         // TODO: 6 - Use logical operators (&&, ||, !) to combine conditions
@@ -41,12 +65,19 @@ public class Operators {
         // Print the result of: hasLicense && hasInsurance
         // Print the result of: hasLicense || hasInsurance
         // Print the result of: !hasLicense
-
+        boolean hasLicense = true;
+        boolean hasInsurance = false;
+        System.out.println("hasLicense && hasInsurance: " + (hasLicense && hasInsurance));
+        System.out.println("hasLicense || hasInsurance: " + (hasLicense || hasInsurance));
+        System.out.println("!hasLicense " + (!hasLicense));
 
         // TODO: 7 - Use the ternary operator to assign "adult" or "minor" based on age
         // Declare an int variable called age with any value
         // Use the ternary operator: String status = (condition) ? "adult" : "minor";
         // Print the status
+        int age = 25;
+        String status = (age > 18) ? "Is an adult" : "Is a minor";
+        System.out.println(status);
 
     }
 }
