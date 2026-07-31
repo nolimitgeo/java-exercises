cbad81928df81571ddeb42f643b714ec0f467a74
nolimitgeo <jobanytorres17@gmail.com>
2026-07-31 08:34:45 -0700

    AccessDemo class implemented; Access Modifiers exercises completed

--- src/main/java/com/amigoscode/_2_developers/_4_accessmodifiers/AccessDemo.java
+++ src/main/java/com/amigoscode/_2_developers/_4_accessmodifiers/AccessDemo.java
@@ -10,25 +10,36 @@ public class AccessDemo {
 
     public static void main(String[] args) {
         // TODO: 1 - Create an instance of AccessModifiers.
+        AccessModifiers access = new AccessModifiers();
 
         System.out.println("=== Calling Public Method ===");
         // TODO: 2 - Call the publicGreeting() method on your instance and print the result.
         //  This works because public methods are accessible from anywhere.
+        System.out.println(access.publicGreeting());
 
         System.out.println("\n=== Calling Package-Private Method ===");
         // TODO: 3 - Call the packageGreeting() method on your instance and print the result.
         //  This works because AccessDemo is in the SAME package as AccessModifiers.
+        System.out.println(access.packageGreeting());
 
         System.out.println("\n=== Accessing Private Field Through Getter ===");
         // TODO: 4 - Use setSecret() to set a value, then use getSecret() to read it back.
         //  Print the result. This demonstrates proper encapsulation:
         //  we access the private field only through public getter/setter methods.
+        access.setSecret("Joba");
+        System.out.println(access.getSecret());
 
         System.out.println("\n=== Why Direct Private Access Won't Work ===");
         // TODO: 5 - Write a comment explaining why the following line would NOT compile:
         //  // instance.secret = "trying direct access";
         //  Then leave the line commented out, preceded by your explanation.
         //  Hint: think about what 'private' means for field visibility outside the class.
+
+        // The reason why the line aforementioned (instance.secret ...) would not work
+        // is due to the instance variable being private.
+        // Because it is private, it can only be accessed directly inside the AccessModifiers class.
+        // This is why we use 'getters' and 'setters', because these are methods WITHIN its scope.
+
 
     }
 }
